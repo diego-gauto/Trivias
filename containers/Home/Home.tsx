fae002b7f82073c8c3cb081cee05861abd39dc08
Andrei <andreiwoolfolk95@gmail.com>
2022-07-25 15:10:15 -0700

    merge fix 1

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -1,14 +1,26 @@
-import { NavBar } from "../../components/NavBar/NavBar";
-
+import React from "react";
+import { ReactElement } from "react";
+// import { NavBar } from "../../components/NavBar/NavBar";
+import { Module1 } from "../../components/Home/Module1/Module1";
+import { Module2 } from "../../components/Home/Module2/Module2";
+import { Module3 } from "../../components/Home/Module3/Module3";
+import { Module5 } from "../../components/Home/Module5/Module5";
+import { Module6 } from "../../components/Home/Module6/Module6";
+import { Module4_Carousel } from "../../components/Home/Module4_Carousel/Module4_Carousel";
+import { Container } from "react-bootstrap";
+import Landings from "../../pages/Screens/Landings";
 /**
  * The home page.
- * @returns {ReactElement} The home page.
+ *
  */
-const HomeContainer = () => {
+//Module 1: Panel de iconos, imagenes y texto
+//Module 2: Panel de iconos y texto
+//Module 3: Informacion principal del curso y es imagen estatica
+//Module 4: Carrusel de lecciones particulares del curso
+export const HomeContainer = () => {
   return <>
-    <NavBar title={"Gonvar Academy"} />
+    <Landings />
   </>
-
 };
 
 export default HomeContainer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AppProps } from "next/dist/shared/lib/router/router";
-
+import Layout from "../components/Layout";
+import "./styles.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
-
 export default MyApp;
