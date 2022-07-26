ffa6c0599ef465f644a048e542afa7b7c49eaa90
ricardo <rm09961219@gmail.com>
2022-07-26 09:06:25 -0700

    fix rewards error

--- pages/Screens/Rewards.tsx
+++ pages/Screens/Rewards.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import { MainContain } from '../../screens/Styles.styled';
-import Rewards from '../../containers/Profile/Rewards/Rewards';
+
+
+import Rewards from "../../containers/Profile/Rewards/Rewards";
+import { MainContain } from "../../screens/Styles.styled";
+
 const Landings = () => {
   return (
 
