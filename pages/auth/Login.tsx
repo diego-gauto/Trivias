188d767dcd59f27d95a988d98263580d1e35512f
Axel Octavio Meinguer Estrada <48524504+tawicornell@users.noreply.github.com>
2022-07-28 23:54:52 -0500

    polishing auth
    1)Navbar cuando no tienes perfil
    marca_de_verificación_blanca
    ojos
    manos_levantadas
    2)Registrarse/ iniciar sesión mandar a preview
    3)Loader al iniciar sesión o register
    4)No mostrar validaciones al darle con auth de google o auth fb

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -49,6 +49,8 @@ type FormValues = {
 
 const Login = () => {
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const [passwordShown_1, setPasswordShown_1] = useState(false);
   const togglePassword_1 = () => {
     setPasswordShown_1(!passwordShown_1);
@@ -65,6 +67,7 @@ const Login = () => {
   });
 
   const onSubmit: SubmitHandler<FormValues> = formData => {
+    setIsLoading(true)
     let signUpData = {
       credentials: {
         email: formData.email,
@@ -78,107 +81,123 @@ const Login = () => {
 
 
   const handleSignUpWithAuthProvider = (authProvider: string) => {
-
+    setIsLoading(true)
     accessWithAuthProvider(authProvider).then(() => {
       window.location.href = "/Preview";
     });
   };
 
+
+  useEffect(() => { }, [isLoading]);
+
   return (
-    <Background>
 
-      <LoginBox>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <ProfilePicture />
-          <Title>
-            Iniciar Sesión
-          </Title>
-          <Box1>
-            <Text2>
-              Correo electrónico
-            </Text2>
-            <TextInput
-              type="text"
-              placeholder="correo@correo.com"
-              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              {...register("email")}
-            />
-            <div className="invalid-feedback">
-              {errors.email?.message}
-            </div>
+    <>
+      {!isLoading ? (
+        <Background>
 
-          </Box1>
-          <Box2>
-            <Text2>
-              Contraseña
-            </Text2>
-            <PasswordBox>
-
-              <div>
-                <TextInput_2
-                  type={passwordShown_1 ? "text" : "password"}
-                  placeholder="Contraseña"
-                  className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                  {...register("password")}
+          <LoginBox>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <ProfilePicture />
+              <Title>
+                Iniciar Sesión
+              </Title>
+              <Box1>
+                <Text2>
+                  Correo electrónico
+                </Text2>
+                <TextInput
+                  type="text"
+                  placeholder="correo@correo.com"
+                  className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                  {...register("email")}
                 />
+                <div className="invalid-feedback">
+                  {errors.email?.message}
+                </div>
 
-                <div
-                  onMouseDown={togglePassword_1}
-                  onMouseUp={togglePassword_1}
-                ><EyeIcon ></EyeIcon></div>
-              </div>
-              <div className="invalid-feedback">
-                {errors.password?.message}
-              </div>
+              </Box1>
+              <Box2>
+                <Text2>
+                  Contraseña
+                </Text2>
+                <PasswordBox>
 
-            </PasswordBox>
+                  <div>
+                    <TextInput_2
+                      type={passwordShown_1 ? "text" : "password"}
+                      placeholder="Contraseña"
+                      className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                      {...register("password")}
+                    />
 
-          </Box2>
+                    <div
+                      onMouseDown={togglePassword_1}
+                      onMouseUp={togglePassword_1}
+                    ><EyeIcon ></EyeIcon></div>
+                  </div>
+                  <div className="invalid-feedback">
+                    {errors.password?.message}
+                  </div>
 
-          <AllButtons>
-            <PurpleButton2 type='submit'>
-              Acceder
-            </PurpleButton2>
-          </AllButtons>
+                </PasswordBox>
 
-        </form>
+              </Box2>
 
-        <AllButtons>
+              <AllButtons>
+                <PurpleButton2 type='submit'>
+                  Acceder
+                </PurpleButton2>
+              </AllButtons>
 
-          <GoogleButton onClick={() => {
-            handleSignUpWithAuthProvider("Google");
-          }}
-          >
-            <GoogleIcon></GoogleIcon>
-            Acceder con Google
-          </GoogleButton>
-          <FacebookButton
-            onClick={() => {
-              handleSignUpWithAuthProvider("Facebook");
-            }}
-          >
-            <FacebookIcon></FacebookIcon>
-            Acceder con Facebook
-          </FacebookButton>
-        </AllButtons>
-        <Text3>
-          ¿Olvidaste tu contraseña? &nbsp;
-          <LinkText>
-            Clic aqui
-          </LinkText>
-        </Text3>
-        <Text3>
-          ¿Es tu primera vez con nosotros? &nbsp;
-          <LinkText>
-            Registrate
-          </LinkText>
-        </Text3>
-      </LoginBox>
-      <GradientCanvas id="gradient-canvas" increasedHeight />
-    </Background >
+            </form>
 
+            <AllButtons>
+
+              <GoogleButton onClick={() => {
+                handleSignUpWithAuthProvider("Google");
+              }}
+              >
+                <GoogleIcon></GoogleIcon>
+                Acceder con Google
+              </GoogleButton>
+              <FacebookButton
+                onClick={() => {
+                  handleSignUpWithAuthProvider("Facebook");
+                }}
+              >
+                <FacebookIcon></FacebookIcon>
+                Acceder con Facebook
+              </FacebookButton>
+            </AllButtons>
+            <Text3>
+              ¿Olvidaste tu contraseña? &nbsp;
+              <LinkText>
+                Clic aqui
+              </LinkText>
+            </Text3>
+            <Text3>
+              ¿Es tu primera vez con nosotros? &nbsp;
+              <LinkText>
+                Registrate
+              </LinkText>
+            </Text3>
+          </LoginBox>
+          <GradientCanvas id="gradient-canvas" increasedHeight />
+        </Background >
+
+      ) : (
+
+        <Background>
+
+          <h1>{"Cargando..."} </h1>
+
+        </Background>
+      )}
+
+    </>
   )
 }
 export default Login;
