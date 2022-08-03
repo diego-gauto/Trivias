8464d0c25ad96df996e91e279743cb32d26c0089
ricardo <rm09961219@gmail.com>
2022-08-03 12:43:53 -0700

    Add loader to project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
+
+import { Background, LoaderContain, LoaderImage } from "../screens/Login.styled";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 
 const Layout = ({ children }: any) => {
 
+  const [isLoading, setIsLoading] = useState(true);
+  //useEffect(() => { }, [isLoading]);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000)
+  }, []);
 
   return (
     <>
-      <NavBar />
-      {children}
-      <Footer />
+      {!isLoading ? (
+        <>
+          <NavBar />
+          {children}
+          <Footer />
+        </>
+
+      ) : (
+        <Background>
+          <LoaderImage>
+            <LoaderContain />
+          </LoaderImage>
+        </Background>
+      )}
     </>
   )
 
