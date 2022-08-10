c3151d58d40f6699351c7b9dcd5acbcd3509f6a0
Ricardo Medina <rm09961219@gmail.com>
2022-08-09 22:55:53 -0700

    Add hls video

--- components/Catalogue/Module1/Module1.tsx
+++ components/Catalogue/Module1/Module1.tsx
@@ -1,4 +1,4 @@
-
+import ReactPlayer from "react-player";
 
 import {
   ButtonContain,
@@ -14,26 +14,32 @@ import {
 } from "./Module1.styled";
 
 const Module1 = () => {
+
   return (
     <Container>
       <ImageContain>
-        {/* <Banner
-          src="/images/Preview/fondo1.png"
-          layout="fill"
-          priority
-        /> */}
+
         <VideoContain>
-          <video style={{ width: "100vw", }} autoPlay loop muted playsInline >
-            <source src="/images/Video/video1.mp4" type="video/mp4"></source>
-          </video>
+          <ReactPlayer
+            className='absolute'
+            url='https://cadefivideo.com.mx/media/2022/JUNIO/COMPLIANCE/master.m3u8'
+            playing={true}
+            muted={true}
+            //controls
+            width='100vw'
+            height='100%'
+          />
+          {/* <video style={{ width: "100vw", }} autoPlay loop muted playsInline >
+            <source src="https://cadefivideo.com.mx/media/2022/JUNIO/COMPLIANCE/master.m3u8" type="application/x-mpegURL"></source>
+          </video> */}
         </VideoContain>
       </ImageContain>
 
       <TextContain>
-        <Title>
+        <Title style={{ textShadow: "1px 1px 10px black" }}>
           Curso 1: Episodio 05 “El Regreso”
         </Title>
-        <SubText>
+        <SubText style={{ textShadow: "1px 1px 10px black" }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus adipiscing amet, enim quis. Quis massa tempus felis id tellus nunc, eu.
         </SubText>
         <ButtonContain>
