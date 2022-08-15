c3eae5e3a3ed835cedc0fda0dc77c3d20da350cd
andreiwoolfolk95 <91649448+andreiwoolfolk95@users.noreply.github.com>
2022-08-14 22:17:17 -0700

    admin prizes

--- components/admin/Rewards/Prizes/Modal/Modal1.tsx
+++ components/admin/Rewards/Prizes/Modal/Modal1.tsx
@@ -1,6 +1,8 @@
 
 
+import { useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
+import { updateRewards } from "../../../../../store/actions/RewardActions";
 
 import { CloseIcon } from "../../../Users/UserData/AllUsers.styled";
 import {
@@ -18,7 +20,28 @@ import {
   TitleContain,
 } from "./Modal.styled";
 
-const Modal1 = ({ show, setShow }: any) => {
+const Modal1 = ({ show, setShow, data }: any) => {
+  const [reward, setReward] = useState<any>({ data })
+  const getImage = (file: any) => {
+    console.log(file)
+    var reader = new FileReader();
+    reader.readAsDataURL(file[0]);
+    reader.onload = (_event) => {
+      setReward({ ...reward, format: reader.result })
+    };
+  }
+  const update = () => {
+    if (Object.keys(reward).some(key => reward[key] === '')) {
+      alert("Complete todos los campos")
+    }
+    else {
+      updateRewards(reward, reward.id);
+      console.log(reward)
+    }
+  }
+  useEffect(() => {
+    setReward({ ...data })
+  }, [data])
 
   const handleClose = () => setShow(false);
   return (
@@ -30,25 +53,49 @@ const Modal1 = ({ show, setShow }: any) => {
         </TitleContain>
         <InputContain>
           <Label>Nombre de la Recompensa</Label>
-          <Input placeholder="Gonvar Nails Leonardo Da Vinci" />
+          <Input
+            placeholder="Gonvar Nails Leonardo Da Vinci"
+            defaultValue={data.title}
+            onChange={(e) => {
+              setReward({ ...reward, title: e.target.value })
+            }}
+          />
         </InputContain>
         <InputContain>
           <Label>Puntos</Label>
-          <Input placeholder="1100" />
+          <Input
+            placeholder="1100"
+            defaultValue={reward.points}
+            onChange={(e) => {
+              setReward({ ...reward, points: e.target.value })
+            }}
+          />
         </InputContain>
         <InputContain>
           <Label>Descripción</Label>
-          <InputBig placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus ultrices id feugiat cursus velit. Aliquam pulvinar in orci malesuada. Pellentesque aliquam aliquam nulla sodales tortor pretium aliquet ultricies. Interdum et suspendisse nunc gravida. " />
+          <InputBig
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus ultrices id feugiat cursus velit. Aliquam pulvinar in orci malesuada. Pellentesque aliquam aliquam nulla sodales tortor pretium aliquet ultricies. Interdum et suspendisse nunc gravida. "
+            defaultValue={reward.about}
+            onChange={(e) => {
+              setReward({ ...reward, about: e.target.value })
+            }}
+          />
         </InputContain>
         <InputContain>
           <Label>Imagen del Producto</Label>
           <IconContain>
             <Folder />
-            <Input2 placeholder="Seleccionar archivo" />
+            <Input2
+              type="file"
+              placeholder="Seleccionar archivo"
+              onChange={(e) => { getImage(e.target.files) }}
+            />
           </IconContain>
         </InputContain>
         <ButtonContain>
-          <Button>Guardar</Button>
+          <Button onClick={() => {
+            update()
+          }}>Guardar</Button>
         </ButtonContain>
       </ModalContain>
     </Modal>
--- store/actions/RewardActions.tsx
+++ store/actions/RewardActions.tsx
@@ -2,7 +2,8 @@ import {
   collection, doc, getDocs, getFirestore, query, setDoc, addDoc, where, onSnapshot, updateDoc, deleteDoc,
 } from "firebase/firestore";
 import { db } from '../../firebase/firebaseConfig';
-
+import { v4 as uuidv4 } from 'uuid';
+import { getStorage, ref, getDownloadURL, uploadString, deleteObject } from "firebase/storage";
 export const deleteLevel = async (level: any) => {
   await deleteDoc(doc(db, "levelPoints", level.id));
 }
@@ -22,5 +23,66 @@ export const updateLevel = async (level: any, id: any) => {
     maximum: level.maximum,
     minimum: level.minimum
   })
+  return 'exito'
+}
+
+export const addReward = async (reward: any) => {
+  console.log(reward)
+  reward.reference = `${reward.title}-${uuidv4()}`
+  reward.path = await uploadImage(reward.path, reward.reference);
+
+  console.log(reward)
+  const docRef = await addDoc(
+    collection(db, "rewards"),
+    {
+      ...reward
+    }
+  );
+  return 'exito'
+}
+
+const uploadImage = (image: any, name: any) => {
+  const storage = getStorage();
+  const storageRef = ref(storage, `rewards/${name}`);
+  return new Promise((resolve, reject) => {
+    uploadString(storageRef, image, 'data_url').then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        resolve(downloadURL)
+      });
+    });
+  });
+}
+export const getRewards = async () => {
+  let data: any = []
+  const docRef = collection(db, 'rewards');
+  const querySnapshot = await getDocs(docRef);
+  querySnapshot.forEach((doc) => {
+    data.push({ ...doc.data(), id: doc.id })
+  });
+  return data
+}
+
+export const updateRewards = async (reward: any, id: any) => {
+  let tempReward: any = JSON.parse(JSON.stringify(reward))
+  const storage = getStorage();
+  const desertRef = ref(storage, `rewards/${tempReward.reference}`);
+  if ("format" in tempReward) {
+    await deleteObject(desertRef).then(async () => {
+      tempReward.reference = `${tempReward.title}-${uuidv4()}`
+    }).catch((error) => {
+      console.log(error)
+    });
+    tempReward.path = await uploadImage(tempReward.format, tempReward.reference);
+    delete tempReward.format;
+  }
+  console.log(tempReward)
+  console.log(id)
+  const docRef = doc(db, 'rewards', id);
+  delete tempReward.id;
+
+  await updateDoc(docRef, {
+    ...tempReward
+  })
+
   return 'exito'
 }
