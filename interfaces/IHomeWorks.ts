6ac6b18afc7ce89c6ea9a453e2d1a349455b102e
andreiwoolfolk95 <91649448+andreiwoolfolk95@users.noreply.github.com>
2022-09-08 13:56:36 -0700

    interfaceHomeWork

---
+++ interfaces/IHomeWorks.ts
@@ -0,0 +1,11 @@
+export interface HomeWorkModal {
+  id: string,
+  lesson: number,
+  season: number,
+  status: boolean,
+  title: string,
+  userId: string,
+  courseId: string,
+  userEmail: string,
+  userName: string,
+}
