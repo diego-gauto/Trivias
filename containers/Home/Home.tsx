f12bc5379fe305cda8dc5568536f324b58e98294
ivansmorales <ivann_mg@hotmail.com>
2022-10-03 11:25:03 -0500

    Fix build issues

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import { ReactElement } from "react";
-// import { NavBar } from "../../components/NavBar/NavBar";
-import { Module1 } from "../../components/Home/Module1/Module1";
-import { Module2 } from "../../components/Home/Module2/Module2";
-import { Module3 } from "../../components/Home/Module3/Module3";
-import { Module5 } from "../../components/Home/Module5/Module5";
-import { Module6 } from "../../components/Home/Module6/Module6";
-import { Module4_Carousel } from "../../components/Home/Module4_Carousel/Module4_Carousel";
-import { Container } from "react-bootstrap";
+
+
+
+
 import Landings from "../../pages/index";
+
 /**
  * The home page.
  *
