81028d8d16473d7f3ae644079681e3cc0062916d
ivansmorales <ivann_mg@hotmail.com>
2022-10-05 21:03:02 -0500

    Add dynamic carousel to index

--- components/Home/Module4_Carousel/SlideModule/SlideModule.styled.ts
+++ components/Home/Module4_Carousel/SlideModule/SlideModule.styled.ts
@@ -1,94 +1,87 @@
-
-
 import styled from "styled-components";
 
-export const SlideImg = styled.div ` 
-width: 100%;
-height: 200px;
-border-radius: 10px;
-background-repeat: no-repeat;
-background-position: center;
-background-size: 100% auto;
-padding: 0;
-border-radius: 20px;
-@media only screen and (max-width: 1024px) {
-  margin-bottom: 15px;
-  height: 60vh;
-}
-@media only screen and (max-width: 660px) {
-  margin-bottom: 15px;
-  margin-top: 0px;
-  height: 22vh;
-  background-size: 95% auto;
-}
-@media only screen and (max-width: 500px) {
-  margin-bottom: 15px;
-  margin-top: 0px;
-  height: 16vh;
-}
+export const SlideImg = styled.div`
+  width: 95%;
+  height: 200px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% auto;
+  padding: 0;
+  @media only screen and (max-width: 1024px) {
+    margin-bottom: 15px;
+    height: 60vh;
+  }
+  @media only screen and (max-width: 660px) {
+    margin-bottom: 15px;
+    margin-top: 0px;
+    height: 22vh;
+    background-size: 95% auto;
+  }
+  @media only screen and (max-width: 500px) {
+    margin-bottom: 15px;
+    margin-top: 0px;
+    height: 16vh;
+  }
 `;
 
-export const TextSectionWrapper = styled.div  `
-padding-bottom: 40px;
-@media only screen and (max-width: 1024px) {
-  display: none;
-  padding-bottom: 0;
-}
+export const TextSectionWrapper = styled.div`
+  padding-bottom: 40px;
+  @media only screen and (max-width: 1024px) {
+    display: none;
+    padding-bottom: 0;
+  }
 `;
 
-export const NewTag = styled.div  `
-margin-top: 9px;
-margin-left: 9px;
-border: 1px solid #FFFFFF;
-border-radius: 10px; 
-width: 79px;
-height: 29px;
-text-align: center;
+export const NewTag = styled.div`
+  margin-top: 9px;
+  margin-left: 9px;
+  border: 1px solid #ffffff;
+  border-radius: 10px;
+  width: 79px;
+  height: 29px;
+  text-align: center;
 `;
 
-export const TextNew = styled.span `
- 
-font-family: 'Raleway';
-font-style: normal;
-font-weight: 400;
-font-size: 14px;
-line-height: 150%; 
-margin-left: 0px;
-color: #FFFFFF; 
-flex: none;
-order: 0;
-flex-grow: 0;
-@font-face{
-  font-family:Raleway;
-  src:url(../fonts/Raleway-VariableFont_wght.ttf);
-}
+export const TextNew = styled.span`
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+  margin-left: 0px;
+  color: #ffffff;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  @font-face {
+    font-family: Raleway;
+    src: url(../fonts/Raleway-VariableFont_wght.ttf);
+  }
 `;
-export const Text01 = styled.span `
- 
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 400;
-font-size: 18px;
-line-height: 150%; 
-color: #000000;
-@font-face{
-  font-family:Montserrat;
-  src:url(../fonts/Montserrat-VariableFont_wght.ttf);
-}  
+export const Text01 = styled.span`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+  color: #000000;
+  @font-face {
+    font-family: Montserrat;
+    src: url(../fonts/Montserrat-VariableFont_wght.ttf);
+  }
 `;
-export const Text02 = styled.span `
- 
-font-family: 'Raleway';
-font-style: normal;
-font-weight: 400;
-font-size: 14px;
-line-height: 150%;
+export const Text02 = styled.span`
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
 
-/* identical to box height, or 21px */
+  /* identical to box height, or 21px */
 
-color: #000000;
-@font-face{
-  font-family:Montserrat;
-  src:url(../fonts/Montserrat-VariableFont_wght.ttf);
-}  
+  color: #000000;
+  @font-face {
+    font-family: Montserrat;
+    src: url(../fonts/Montserrat-VariableFont_wght.ttf);
+  }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 import { Container } from "react-bootstrap";
-import { useMediaQuery } from "react-responsive";
 
 import { GonvarPlusModule } from "../components/Home/GonvarPlusModule/GonvarPlusModule";
 import { Module2 } from "../components/Home/Module2/Module2";
@@ -17,7 +16,7 @@ import {
 import { CourseModuleContainer } from "../containers/Home/CourseModuleContainer/CourseModuleContainer";
 import { FirstSectionContainer } from "../containers/Home/FirstSectionContainer/FirstSectionContainer";
 import { Background, LoaderContain, LoaderImage } from "../screens/Login.styled";
-import { getWholeCourse, getWholeCourses } from "../store/actions/courseActions";
+import { getCourses, getWholeCourse, getWholeCourses } from "../store/actions/courseActions";
 import { getLandingData } from "../store/actions/LandingActions";
 
 const Homepage = () => {
@@ -25,18 +24,18 @@ const Homepage = () => {
   const [landingData, setLandingData] = useState<any>({});
   const [courseData, setCourseData] = useState<any>({});
   const [courses, setCourses] = useState<any>({});
-
-  const responsive380 = useMediaQuery({ query: "(max-width: 390px)" });
-  const responsive520 = useMediaQuery({ query: "(max-width: 520px)" });
-  const responsive600 = useMediaQuery({ query: "(max-width: 600px)" });
-  const responsive800 = useMediaQuery({ query: "(max-width: 800px)" });
-  const responsive1023 = useMediaQuery({ query: "(max-width: 1023px)" });
+  const [courseNailsData, setCourseNailsData] = useState<any>({});
+  const [courseSEPData, setCourseSEPData] = useState<any>({});
 
   const fetchLandingData = async () => {
     const landingData = await getLandingData();
     setLandingData(landingData);
     const courseData = await getWholeCourse('dvns4pbd0ZHjFqR9VMks');
     setCourseData(courseData);
+    const courseNailsData = await getWholeCourse(NAILS_MASTER_COURSE_ID);
+    setCourseNailsData(courseNailsData);
+    const courseSEPData = await getWholeCourse(SEP_COURSE_ID);
+    setCourseSEPData(courseSEPData);
     setLoading(false);
   }
 
@@ -84,7 +83,12 @@ const Homepage = () => {
       <GonvarPlusModule />
       {courses &&
         <Module4_Carousel isInfinite={true} slideData={
-          courses.map((course: any) => {
+          courses.filter((course: any) =>
+            course.documentID !== NAILS_MASTER_COURSE_ID
+            && course.documentID !== EXPERTS_ESCULTURAL_COURSE_ID
+            && course.documentID !== DRY_MANICURE_COURSE_ID
+            && course.documentID !== SEP_COURSE_ID
+          ).map((course: any) => {
             return (
               { isNew: false, title: course.courseTittle, subtitle: "", imgURL: course.coursePath }
             )
@@ -94,49 +98,42 @@ const Homepage = () => {
 
       {/* Nails Master Module Card */}
       <CourseModuleContainer courseId={NAILS_MASTER_COURSE_ID} />
-
-      {courses &&
+      {courseNailsData &&
         <Module4_Carousel isInfinite={true} slideData={
-          courses.map((course: any) => {
+          courseNailsData.lessons.map((lesson: any) => {
             return (
-              { isNew: false, title: course.courseTittle, subtitle: "", imgURL: course.coursePath }
+              { isNew: false, title: lesson.title, subtitle: "", imgURL: lesson.image }
             )
           })
         } />
       }
+
       {/* SEP Module Card */}
-      {/* <CourseModuleContainer courseId="trY3l4ytjrNbCXXnTBew" /> */}
+      <CourseModuleContainer courseId={SEP_COURSE_ID} />
+      {courseSEPData &&
+        <Module4_Carousel isInfinite={true} slideData={
+          courseSEPData.lessons.map((lesson: any) => {
+            return (
+              { isNew: false, title: lesson.title, subtitle: "", imgURL: lesson.image }
+            )
+          })
+        } />
+      }
 
       {/* Dry's Manicure Module Card */}
+      <CourseModuleContainer courseId={DRY_MANICURE_COURSE_ID} />
+      {courses &&
+        <Module4_Carousel isInfinite={true} slideData={
+          courses.map((course: any) => {
+            return (
+              { isNew: false, title: course.courseTittle, subtitle: "", imgURL: course.coursePath }
+            )
+          })
+        } />
+      }
 
       {/* Experts Escultural Module Card */}
-
-      <CourseModuleContainer courseId={SEP_COURSE_ID} />
-
-      {courses &&
-        <Module4_Carousel isInfinite={true} slideData={
-          courses.map((course: any) => {
-            return (
-              { isNew: false, title: course.courseTittle, subtitle: "", imgURL: course.coursePath }
-            )
-          })
-        } />
-      }
-      <CourseModuleContainer courseId={DRY_MANICURE_COURSE_ID} />
-
-
-      {courses &&
-        <Module4_Carousel isInfinite={true} slideData={
-          courses.map((course: any) => {
-            return (
-              { isNew: false, title: course.courseTittle, subtitle: "", imgURL: course.coursePath }
-            )
-          })
-        } />
-      }
-
       <CourseModuleContainer courseId={EXPERTS_ESCULTURAL_COURSE_ID} />
-
       {courses &&
         <Module4_Carousel isInfinite={true} slideData={
           courses.map((course: any) => {
@@ -146,6 +143,7 @@ const Homepage = () => {
           })
         } />
       }
+
       <Module5 reviewsData={landingData.reseniasSectionData} />
       <Module6 slideData={landingData.productosDestacadosData} />
     </Container>
