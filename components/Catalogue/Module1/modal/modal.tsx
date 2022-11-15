a652b6ab3e2c9eda5a611aa332a6bb0cfd8b5bfc
Andrei <andreiwoolfolk95@gmail.com>
2022-11-15 13:35:14 -0700

    GON-022

--- components/Catalogue/Module1/modal/modal.tsx
+++ components/Catalogue/Module1/modal/modal.tsx
@@ -100,6 +100,51 @@ const Modal = ({ show, setShow, course, user }: any) => {
     }
     return result;
   }
+  var xDown: any = null;
+  var yDown: any = null;
+  document.addEventListener('touchstart', handleTouchStart, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
+  function getTouches(evt: any) {
+    return evt.touches ||             // browser API
+      evt.originalEvent.touches; // jQuery
+  }
+  function handleTouchStart(evt: any) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  function handleTouchMove(evt: any) {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    console.log(yDiff)
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+      if (xDiff > 0) {
+        /* right swipe */
+      } else {
+        /* left swipe */
+      }
+    } else {
+      if (yDiff > 0) {
+        /* down swipe */
+        console.log('arriba')
+      }
+      if (yDiff < -350) {
+        /* up swipe */
+        handleClose()
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
 
   return (
     <ModalContain>
