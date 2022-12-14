f9cf5ae68f9651386e35c59a24c4480b34bbf953
andreiwoolfolk95 <91649448+andreiwoolfolk95@users.noreply.github.com>
2022-12-14 16:19:36 -0700

    Certificates

---
+++ pages/Certificates/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MainContain } from '../../screens/Styles.styled';
+const CertificatesScreen = () => {
+  return (
+    <MainContain>
+      <div>a</div>
+    </MainContain>
+  )
+}
+export default CertificatesScreen;
