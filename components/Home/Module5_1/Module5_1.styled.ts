fb2c835cc94d4233b3450743ff09c44922c844d3
andreiwoolfolk95 <91649448+andreiwoolfolk95@users.noreply.github.com>
2022-12-15 10:44:05 -0700

    GON-103

--- components/Home/Module5_1/Module5_1.styled.ts
+++ components/Home/Module5_1/Module5_1.styled.ts
@@ -17,9 +17,9 @@ export const People = styled.div`
   background-position-x: 50%;
   z-index: 1;
 
-  background-size: 25%;
+  background-size: 27%;
   top: 23%;
-  height: 420px;
+  height: 550px;
 
   @media only screen and (max-width: 2400px) {
     background-size: 37%;
@@ -40,7 +40,7 @@ export const People = styled.div`
 
   @media only screen and (max-width: 1720px) {
     background-size: 51%;
-top: -2%;
+    top: -2%;
   }
   @media only screen and (max-width: 1600px) {
     background-size: 44%;
@@ -104,15 +104,13 @@ export const Lines = styled.div`
 `;
 export const BgColor = styled.div`
   background-color: #ede7f2;
-  height: 250px; 
+  height: 250px;
   position: relative;
 
   @media only screen and (max-width: 2400px) {
-   
     top: -55%;
   }
   @media only screen and (max-width: 2100px) {
-   
     top: -62%;
   }
 
@@ -122,28 +120,27 @@ export const BgColor = styled.div`
   @media only screen and (max-width: 1720px) {
     top: -70%;
   }
- 
+
   @media only screen and (max-width: 1600px) {
-    
     height: 190px;
     top: -90%;
   }
- 
+
   @media only screen and (max-width: 1440px) {
-    top: -110%; 
+    top: -110%;
   }
   @media only screen and (max-width: 1200px) {
-    top: -115%; 
+    top: -115%;
     height: 210px;
   }
   @media only screen and (max-width: 1150px) {
-    top: -110%; 
+    top: -110%;
   }
   @media only screen and (max-width: 1080px) {
-    top: -107%; 
+    top: -107%;
   }
   @media only screen and (max-width: 1028px) {
-    top: -110%; 
+    top: -110%;
   }
 `;
 export const TittleA = styled.span`
