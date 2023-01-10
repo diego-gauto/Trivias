46dcabb6e25a7eab6b946d4b3f6f59175039049b
Oscar Quiroz <79659794+kevinhmz@users.noreply.github.com>
2023-01-09 17:01:19 -0700

    Lesson quiz process

--- containers/Profile/Lesson/LessonComponents/Modules/HomeWork.tsx
+++ containers/Profile/Lesson/LessonComponents/Modules/HomeWork.tsx
@@ -2,16 +2,31 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import { addHomework, getHomework } from '../../../../../store/actions/courseActions'
 import { DownlowadContain, DownloadText, FileIcon, Weight, Pdf } from './Extra.styled'
-import { TaskTitle, TaskText, ButtonDiv, UploadButton, UploadIcon, HomeWorkContain, ReviewButton } from './HomeWork.styled'
+import { TaskTitle, TaskText, ButtonDiv, UploadButton, UploadIcon, HomeWorkContain, ReviewButton, Answer } from './HomeWork.styled'
 import { TitleContain, PositionTitle, Titles, ListIcon, BookIcon, ChatboxIcon, EaselIcon, IconContain, SelectContain, UnSelected } from './Module.styled'
 import { BiDownload } from "react-icons/bi";
 import { BsFileArrowUp } from "react-icons/bs";
 import { BsPlayBtn } from 'react-icons/bs';
 import { SlNotebook } from 'react-icons/sl';
 import { TfiCommentAlt } from 'react-icons/tfi';
+import { number } from 'yup';
 
 const HomeWork = ({ value, setValue, data, user, season, lesson, teacherCreds }: any) => {
   const [status, setStatus] = useState("");
+  const [step, setStep] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [answers, setAnswers] = useState<any>([]);
+  const [verify, setVerify] = useState(false);
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    let tempAnswers: any = [];
+    data.quiz.questions.forEach((element: any) => {
+      tempAnswers.push([]);
+    });
+    setAnswers(tempAnswers);
+  }, [])
+
 
   useEffect(() => {
     getHomework(data.id, user.id).then((res: any) => {
@@ -66,6 +81,52 @@ const HomeWork = ({ value, setValue, data, user, season, lesson, teacherCreds }:
     document.getElementById('hide')?.click()
   }
 
+  const addAnswer = (value: number) => {
+    data.quiz.questions[index].answers.forEach((element: any, idxAnswer: number) => {
+      if (value == idxAnswer) {
+        let answerDiv: any = document.getElementById(index + "answer" + idxAnswer)
+        answerDiv.style.background = "black";
+        answerDiv.style.color = "white";
+      } else {
+        let answerDiv: any = document.getElementById(index + "answer" + idxAnswer)
+        answerDiv.style.background = "none";
+        answerDiv.style.color = "black";
+      }
+    });
+
+    answers[index] = value;
+    setAnswers(answers);
+  }
+
+  const checkAnswer = () => {
+    let score = data.quiz.points / data.quiz.questions.length;
+    if (answers[index].length == 0) {
+      alert("Seleccione una opción!")
+    } else {
+      setVerify(true);
+      if (data.quiz.questions[index].answers[answers[index]].status) {
+        let tempPoints = points;
+        tempPoints += score;
+        setPoints(tempPoints);
+      }
+    }
+  }
+
+  const nextQuestion = () => {
+    setVerify(false);
+    setIndex(index + 1);
+    data.quiz.questions[index].answers.forEach((element: any, idxAnswer: number) => {
+      let answerDiv: any = document.getElementById(index + "answer" + idxAnswer)
+      answerDiv.style.background = "none";
+      answerDiv.style.color = "black";
+    });
+  }
+
+  const checkQuiz = () => {
+    setVerify(false);
+    console.log(points);
+  }
+
   return (
     <>
       <TitleContain >
@@ -88,28 +149,7 @@ const HomeWork = ({ value, setValue, data, user, season, lesson, teacherCreds }:
         <div className='line'></div>
       </TitleContain>
       <HomeWorkContain>
-        {/* <div className='left'>
-          <TaskTitle>
-            Material de apoyo
-          </TaskTitle>
-          <ol type="a">
-            {data?.extra?.map((extra: any) => {
-              return (
-                <Link href={extra.path}>
-                  <a target="_blank" style={{ textDecoration: 'none', color: 'black' }}>
-                    <DownlowadContain>
-                      <DownloadText>
-                        <li>{extra.title.slice(0, -4)}</li>
-                      </DownloadText>
-                      <Pdf><BiDownload></BiDownload> Descargar Pdf</Pdf>
-                    </DownlowadContain>
-                  </a>
-                </Link>
-              )
-            })}
-          </ol>
-        </div> */}
-        <div className='right'>
+        {(data.type == "homework" || !("type" in data)) ? <div className='right'>
           <TaskTitle style={{ color: "#f78803" }}>
             Sube aquí tus prácticas <br />
             <span>
@@ -155,7 +195,37 @@ const HomeWork = ({ value, setValue, data, user, season, lesson, teacherCreds }:
             </div>}
           </div> :
             <p>Lección sin tarea...</p>}
-        </div>
+        </div> :
+          <div className='quiz'>
+            {step == 0 && <div className='quiz-info'>
+              <p>Quiz: {data.quiz.title}</p>
+              <p>Para aprobar el quiz es necesario obtener {data.quiz.passingGrade} puntos o más</p>
+              <button onClick={() => { setStep(1) }}>Comenzar quiz</button>
+            </div>}
+            {step == 1 && <div className='question-container'>
+              <div className='question-title'>
+                <p>{index + 1}.</p>
+                <p dangerouslySetInnerHTML={{ __html: data.quiz.questions[index].question }}></p>
+              </div>
+              <div className='answers'>
+                {data.quiz.questions[index].answers.map((x: any, idx: number) => {
+                  return (
+                    <Answer id={index + "answer" + idx} key={"answers" + idx} veryfy={verify} correct={x.status} onClick={() => {
+                      addAnswer(idx);
+                    }}>
+                      {x.answer}
+                    </Answer>
+                  )
+                })}
+              </div>
+              {!verify ? <button onClick={() => {
+                checkAnswer()
+              }}>Verificar</button> :
+                index !== data.quiz.questions.length - 1 ? <button onClick={nextQuestion}>Siguiente</button> :
+                  <button onClick={() => {
+                  }}>Terminar Quiz</button>}
+            </div>}
+          </div>}
       </HomeWorkContain>
     </>
 
