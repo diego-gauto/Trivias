1913bd3c3bf6f4129bf92f086c34d53e7307e401
Andrei <andreiwoolfolk95@gmail.com>
2023-02-24 08:56:43 -0700

    blo pt7

--- containers/Profile/Blog/Blog.tsx
+++ containers/Profile/Blog/Blog.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { BlogContainer, BlogItems } from './Blog.styled';
+import router from "next/router";
+import { getBlogs } from '../../../store/actions/AdminActions';
+import { AiFillPlusCircle } from 'react-icons/ai';
+import { LoaderContain } from '../User/User.styled';
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState<Array<any>>([]);
+  const [loader, setLoader] = useState(false);
+
+  const goToBlog = (blog: any) => {
+    // let blogText: any = blog.title.replaceAll(" ", "-");
+    // router.push({ pathname: "/admin/CreateBlog/", query: { blogId: blog.id } })
+  }
+  const Context = createContext(blogs);
+  console.log(Context)
+  useEffect(() => {
+    getBlogs().then((res: any) => {
+      setBlogs(res)
+      setLoader(true);
+    })
+  }, [router])
   return (
     <BlogContainer>
       <div className="title-contain">
@@ -9,10 +28,43 @@ const Blog = () => {
           Blog
         </p>
       </div>
-      <BlogItems>
-        ded
-      </BlogItems>
+      {
+        loader ?
+          <div className="blogs">
+            {
+              blogs.map((blog: any, index: any) => {
+                return (
+                  <BlogItems key={"blog-card " + index}>
+                    <div className="img-contain" onClick={() => { goToBlog(blog) }}>
+                      <img className="blog-image" src={blog.path} />
+                      <p className="edit-icon"><AiFillPlusCircle /></p>
+                    </div>
+                    <div className="text-contain">
+                      <h1 className="blog-title">
+                        {blog.title}
+                      </h1>
+                      <div className="create-date-contain">
+                        <p className="blog-create">by Academia Gonvar | </p>
+                        <p className="blog-date">Feb 7, 2023</p>
+                      </div>
+                      <div className="last-text">
+                        <h3 className="blog-about">
+                          {blog.subTitle}
+                        </h3>
+                        <a className="read-more" onClick={() => { goToBlog(blog) }}>
+                          Leer mas...
+                        </a>
+                      </div>
+                    </div>
+                  </BlogItems>
+                )
+              })
+            }
+          </div>
+          : <LoaderContain />
+      }
     </BlogContainer>
   )
+
 }
 export default Blog;
