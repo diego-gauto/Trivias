6325349e09fdb4c78acaf189c2c58c24e5562548
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-03-06 17:05:25 -0700

    test 9

--- containers/Profile/Blog/BlogView/HelmetMetaTags/HelmetMetaTags.tsx
+++ containers/Profile/Blog/BlogView/HelmetMetaTags/HelmetMetaTags.tsx
@@ -13,7 +13,27 @@ export default function HelmetMetaTags(props: any) {
   let hashtag = props.hashtag !== undefined ? props.hashtag : "#gonvar";
   return (
     <Helmet>
-      <title>{title}</title>
+
+
+      <title>https://www.gonvar.io/%C2%BFCon-qu%C3%A9-puedo-pegar-u%C3%B1as-postizas-si-no-tengo-pegamento%3F</title>
+      <meta name="description" content="Gonvar Prueba" />
+
+
+      <meta property="og:url" content="https://www.gonvar.io/%C2%BFCon-qu%C3%A9-puedo-pegar-u%C3%B1as-postizas-si-no-tengo-pegamento%3F" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="https://www.gonvar.io/%C2%BFCon-qu%C3%A9-puedo-pegar-u%C3%B1as-postizas-si-no-tengo-pegamento%3F" />
+      <meta property="og:description" content="Gonvar Prueba" />
+      <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/marketing-gonvar.appspot.com/o/blog%2F%C2%BFCon%20qu%C3%A9%20puedo%20pegar%20u%C3%B1as%20postizas%20si%20no%20tengo%20pegamento%3F-604272aa-44aa-4f1e-80b5-7d4588aad96f?alt=media&token=45651d40-7c50-480f-a874-bacbcb65cecf" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="gonvar.io" />
+      <meta property="twitter:url" content="https://www.gonvar.io/%C2%BFCon-qu%C3%A9-puedo-pegar-u%C3%B1as-postizas-si-no-tengo-pegamento%3F" />
+      <meta name="twitter:title" content="https://www.gonvar.io/%C2%BFCon-qu%C3%A9-puedo-pegar-u%C3%B1as-postizas-si-no-tengo-pegamento%3F" />
+      <meta name="twitter:description" content="Gonvar Prueba" />
+      <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/marketing-gonvar.appspot.com/o/blog%2F%C2%BFCon%20qu%C3%A9%20puedo%20pegar%20u%C3%B1as%20postizas%20si%20no%20tengo%20pegamento%3F-604272aa-44aa-4f1e-80b5-7d4588aad96f?alt=media&token=45651d40-7c50-480f-a874-bacbcb65cecf" />
+
+
+      {/* <title>{title}</title>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="csrf_token" content="" />
       <meta property="type" content="website" />
@@ -36,7 +56,7 @@ export default function HelmetMetaTags(props: any) {
       <meta content="image/*" property="og:image:type" />
       <meta property="og:url" content={currentUrl} />
       <meta property="og:site_name" content="CampersTribe" />
-      <meta property="og:description" content={description} />
+      <meta property="og:description" content={description} /> */}
     </Helmet>
   );
 }
