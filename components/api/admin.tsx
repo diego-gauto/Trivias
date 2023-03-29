2c4369524cfd249117e76da8eb8e9a353c5c2218
Oscar Quiroz <79659794+kevinhmz@users.noreply.github.com>
2023-03-29 11:22:07 -0700

    Admin sections

--- components/api/admin.tsx
+++ components/api/admin.tsx
@@ -11,3 +11,28 @@ export const getAdmins = async () => {
       return error
     });
 };
+
+export const updateAdminRole = async (userId: string) => {
+  let user = { id: userId }
+  return axios
+    .put("http://94.74.77.165/" + "admin/update-role", user)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.log(error);
+      return error
+    });
+};
+
+export const updateAdminAccessApi = async (data: any) => {
+  return axios
+    .put("http://94.74.77.165/" + "admin/update-access", data)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.log(error);
+      return error
+    });
+};
