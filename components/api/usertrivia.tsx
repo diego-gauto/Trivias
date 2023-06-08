d3c6a1d09518fc2e221d23380358b921632a0430
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-06-08 13:19:54 -0700

    emailtrivia

--- components/api/usertrivia.tsx
+++ components/api/usertrivia.tsx
@@ -11,3 +11,14 @@ export const userTrivia = async (user: any) => {
       return error
     });
 };
+export const emailTrivia = async (user: any) => {
+  return axios
+    .post("https://gonvar.inowu.dev/email/trivias", user)
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      console.log(error);
+      return error
+    });
+};
