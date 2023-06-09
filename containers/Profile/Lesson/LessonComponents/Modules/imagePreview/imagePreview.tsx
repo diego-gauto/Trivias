fd8370bb677e5926c7249a26e14fd5005f0ea157
AaronRInowu <aaron.robles@inowu.dev>
2023-06-09 16:54:33 -0700

    Image preview resp

--- containers/Profile/Lesson/LessonComponents/Modules/imagePreview/imagePreview.tsx
+++ containers/Profile/Lesson/LessonComponents/Modules/imagePreview/imagePreview.tsx
@@ -1,47 +1,79 @@
-import React from 'react'
-import { Modal } from 'react-bootstrap';
-import { ImageContainter } from './ImagePreview.styled';
-import { AiOutlineClose } from 'react-icons/ai';
-import DocViewer from "react-doc-viewer";
-import { LoaderContain, LoaderImage } from '../../../../../../components/Loader.styled';
+
+
+import { Modal } from "react-bootstrap";
+import { AiOutlineClose } from "react-icons/ai";
+
+import { LoaderImage } from "../../../../../../components/Loader.styled";
+import lesson from "../../../../../../pages/lesson";
+import { LoaderContain } from "../../../../User/User.styled";
+import { ImageContainter } from "./ImagePreview.styled";
 
 const ImagePreview = (props: any) => {
-  const { show, setShow, imageDisplay, type, getImage, loader, setTypeFile, setImageDisplay } = props;
+  console.log(props)
+  const { show, setShow, imageDisplay, type, getImage, loader, setTypeFile, setImageDisplay, user, lesson } = props;
   const handleClose = () => {
     setShow(false);
     setTypeFile('');
     setImageDisplay('');
   }
+  console.log(type)
   return (
     <Modal show={show} centered className='modal-xl'>
       <ImageContainter>
-        <div className='button-contain'>
-          <h1>¿Desea continuar?</h1>
-          {
-            (type !== "aplication/pdf" && type.substring(0, 5) !== "image") &&
-            <p className='note'>(Los documentos .doc y .docx no cuentan con vista previa)</p>
-          }
-          <div className='buttons'>
-            <button className='cancel-btn' onClick={handleClose}>Cancelar</button>
+
+        <AiOutlineClose onClick={handleClose} className='close' />
+        <div className="row">
+          <div className='text-contain col-lg-5 col-md-12'>
+            <h2 className="purple h1 fw-semibold text-center">¡Hola <b>{user}</b>!</h2>
+            <p className="purple h2 fw-semibold text-center">Hermosa, estás por entregar tu tarea de <b>{lesson}</b></p>
+            <p className="purple h2 fw-semibold text-center">Si todo está bien, solo da click en aceptar para enviarla.</p>
             {
-              loader
-                ?
-                <LoaderImage>
-                  <LoaderContain />
-                </LoaderImage>
-                : <button className='continue-btn' onClick={() => getImage(imageDisplay)} >Continuar</button>
+              (type !== "aplication/pdf" && type.substring(0, 5) !== "image") &&
+              <p className='note'>(Los documentos .doc y .docx no cuentan con vista previa)</p>
+            }
+            <div className="button-contain">
+              <div className='buttons'>
+                <button className='btn' onClick={handleClose}>Salir</button>
+                {
+                  loader
+                    ?
+                    <LoaderImage>
+                      <LoaderContain />
+                    </LoaderImage>
+                    //onClick={() => getImage(imageDisplay)}
+                    : <button className='btn'  >Aceptar</button>
+                }
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-7 col-md-12">
+            {
+              type === "application/pdf" &&
+              <iframe src={imageDisplay} className='image' height="600px" />
+            }
+            {
+              type.substring(0, 5) === "image" &&
+              <img src={imageDisplay} className="image" />
             }
           </div>
+          <div className="button-contain">
+            <div className='buttons mt-3'>
+              <button className='btn-down' onClick={handleClose}>Salir</button>
+              {
+                loader
+                  ?
+                  <LoaderImage>
+                    <LoaderContain />
+                  </LoaderImage>
+                  //onClick={() => getImage(imageDisplay)}
+                  : <button className='btn-down'  >Aceptar</button>
+              }
+            </div>
+          </div>
         </div>
-        <AiOutlineClose onClick={handleClose} className='close' />
-        {
-          type === "application/pdf" &&
-          <iframe src={imageDisplay} className='image' height="600px" />
-        }
-        {
-          type.substring(0, 5) === "image" &&
-          <img src={imageDisplay} className="image" />
-        }
+
+
+
       </ImageContainter>
     </Modal>
   )
