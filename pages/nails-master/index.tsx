a53aeb6beb980174c46cf41b468d39b55432f020
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-07-11 13:24:22 -0700

    landing

---
+++ pages/nails-master/index.tsx
@@ -0,0 +1,17 @@
+import LandingNailsMaster from "../../components/LandingNailsMaster/LandingNailsMaster";
+import { MainContain } from "../../screens/Styles.styled";
+
+const termsConditions = () => {
+  return (
+
+    <MainContain
+      style={{
+        width: "100%",
+        padding: "0",
+        maxWidth: "100% !important",
+      }}>
+      <LandingNailsMaster />
+    </MainContain>
+  )
+}
+export default termsConditions;
