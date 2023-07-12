fac7aa9a0fefb28c1c74ef4a70fe7a4fc02743c7
Andrei <andreiwoolfolk95@gmail.com>
2023-07-12 09:32:22 -0700

    rewards

--- pages/Rewards/index.tsx
+++ pages/Rewards/index.tsx
@@ -1,21 +1,22 @@
 import Rewards from "../../containers/Profile/Rewards/Rewards";
 import { MainContain } from "../../screens/Styles.styled";
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-type Repo = {
-  data: [],
-}
-export const getServerSideProps: GetServerSideProps<{ rewards: Repo }> = async ({ req, res }: any) => {
-  const result = await fetch("https://gonvar.inowu.dev/" + "rewards/getRewards");
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
-  const rewards = await result.json();
-  return {
-    props: { rewards }
-  }
-}
-const Landings = ({ rewards }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+// import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+// type Repo = {
+//   data: [],
+// }
+// export const getServerSideProps: GetServerSideProps<{ rewards: Repo }> = async ({ req, res }: any) => {
+//   const result = await fetch("https://gonvar.inowu.dev/" + "rewards/getRewards");
+//   res.setHeader(
+//     'Cache-Control',
+//     'public, s-maxage=10, stale-while-revalidate=59'
+//   )
+//   const rewards = await result.json();
+//   return {
+//     props: { rewards }
+//   }
+// }
+// { rewards }: InferGetServerSidePropsType<typeof getServerSideProps>
+const Landings = () => {
   return (
     <MainContain
       style={{
