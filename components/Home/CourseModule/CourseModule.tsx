172d70891db1f55253b369beadd8fff83ade0484
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-08-08 13:15:57 -0700

    changes

--- components/Home/CourseModule/CourseModule.tsx
+++ components/Home/CourseModule/CourseModule.tsx
@@ -2,7 +2,7 @@ import { Button, Card, Col, Row } from "react-bootstrap";
 import ReactPlayer from "react-player";
 import { useMediaQuery } from "react-responsive";
 import { useRouter } from "next/router";
-import { LESSON_PATH, NAILS_FORM, PREVIEW_PATH, PURCHASE_PATH, SIGNUP_PATH } from "../../../constants/paths";
+import { LESSON_PATH, NAILS_FORM, NAILS_LANDING_REDIRECT, PREVIEW_PATH, PURCHASE_PATH, SIGNUP_PATH } from "../../../constants/paths";
 import { PurpleButton } from "../../common/PurpleButton/PurpleButton";
 import { WhiteButton } from "../../common/WhiteButton/WhiteButton";
 import { CardContainer } from "./CourseModule.styled";
@@ -19,7 +19,8 @@ export const CourseModule = (props: ICourseModuleProps) => {
   const router = useRouter();
   const [show, setShow] = useState(false);
   const handleShow = () => {
-    setShow(true);
+    // setShow(true);
+    router.push({ pathname: NAILS_LANDING_REDIRECT })
   }
 
   const doVideoStuff = () => {
