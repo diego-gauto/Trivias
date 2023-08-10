6a91a0bfff1c11f3c0f1d6e64e9d3bc63fcaa364
Andrei <andreiwoolfolk95@gmail.com>
2023-08-09 22:40:29 -0700

    form

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,8 +61,9 @@ const Homepage = ({ courses }: InferGetServerSidePropsType<typeof getServerSideP
           setCourseGonvarPlus(data.gonvar_courses);
           data.nails_master.totalDuration = hms(data.nails_master.totalDuration);
           setCourseNailsData(data.nails_master);
-          data.alineacion_cert.totalDuration = hms(data.alineacion_cert.totalDuration)
-          setCourseSEPData(data.alineacion_cert);
+          // console.log(data.nails_master);
+          // data.alineacion_cert.totalDuration = hms(data.alineacion_cert.totalDuration)
+          // setCourseSEPData(data.alineacion_cert);
           setLoading(true);
         })
       })
@@ -76,8 +77,8 @@ const Homepage = ({ courses }: InferGetServerSidePropsType<typeof getServerSideP
         setCourseGonvarPlus(data.gonvar_courses);
         data.nails_master.totalDuration = hms(data.nails_master.totalDuration);
         setCourseNailsData(data.nails_master);
-        data.alineacion_cert.totalDuration = hms(data.alineacion_cert.totalDuration)
-        setCourseSEPData(data.alineacion_cert);
+        // data.alineacion_cert.totalDuration = hms(data.alineacion_cert.totalDuration)
+        // setCourseSEPData(data.alineacion_cert);
         setLoading(true);
       })
     }
