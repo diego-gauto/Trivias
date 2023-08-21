0315c8953a4d0e6bd3a99be978e5a11c2330211f
Oscar Quiroz <79659794+kevinhmz@users.noreply.github.com>
2023-08-21 13:23:50 -0700

    notifications

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -308,7 +308,7 @@ const NavBar = () => {
                 <HoverText className="hover-text">Recompensas</HoverText>
               </div>
             </Link>
-            <div className="bell-contain">
+            {/* <div className="bell-contain">
               <SlBell className="bell" onClick={openNotifications} />
               {
                 unReadNotification > 0 &&
@@ -361,7 +361,7 @@ const NavBar = () => {
                 !openNotification &&
                 <HoverText className="hover-text" style={{ top: 39 }}>Notificaciones</HoverText>
               }
-            </div>
+            </div> */}
             <Link href={PROFILE_PATH}>
               < UserImage>
                 {
