8575f67448e8fd33fe7264606bcdae639fd8689c
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-08 11:52:01 -0700

    landing

---
+++ pages/suscripcion-anual-fb/index.tsx
@@ -0,0 +1,39 @@
+
+import { renderToString } from "react-dom/server";
+
+import LandingSuscription from "../../components/LandingSuscription/LandingSuscription";
+import { MainContain } from "../../screens/Styles.styled";
+
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+type Repo = {
+  data: [],
+}
+export const getServerSideProps = async ({ req, res }: any) => {
+  // const result = await fetch("https://gonvar.inowu.dev/" + "courses/getCourses");
+  const html = renderToString(<LandingSuscription price={"$1,599 MXN/anual"} isMonth={false} />)
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  )
+  // let anual = await result.json();
+  let anual = html;
+  return {
+    props: { anual }
+  }
+}
+const termsConditions = ({ anual }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const price = "$1,599 MXN/anual"
+  const month = false;
+  return (
+    <MainContain
+      style={{
+        width: "100%",
+        padding: "0",
+        maxWidth: "100% !important",
+      }}>
+      <LandingSuscription price={price} isMonth={month} isFacebook={true} />
+    </MainContain>
+  )
+}
+export default termsConditions;
