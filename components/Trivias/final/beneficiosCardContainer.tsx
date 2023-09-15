bfedf73b396b241d2a3f2386b2c70e97d9550961
Diego Gauto <diegogauto@yahoo.com.ar>
2023-09-15 09:38:09 -0300

    price fixed in beneficios card

--- components/Trivias/final/beneficiosCardContainer.tsx
+++ components/Trivias/final/beneficiosCardContainer.tsx
@@ -7,7 +7,7 @@ export default function BeneficiosCardContainer() {
 
   const cardInfo = [
     { img: "/images/trivias/Beneficios/asesorias.png", title: "Asesorías ilimitadas ", description: "con nuestros instructores certificados.", extra: "" },
-    { img: "/images/trivias/Beneficios/envio.png", title: "Envío gratis ", description: "en compras superiores a $1,000.00 mx de productos Gonvar.", extra: "" },
+    { img: "/images/trivias/Beneficios/envio.png", title: "Envío gratis ", description: "en compras superiores a $1,000.00 MXN de productos Gonvar.", extra: "" },
     { img: "/images/trivias/Beneficios/certificado.png", title: "Certificación oficial FUV* ", description: "por cada curso que completes.", extra: "*(folio único verificado)" },
     { img: "/images/trivias/Beneficios/puntos.png", title: "Sistema de puntos acumulables ", description: "que podrás cambiar por premios.", extra: "" },
     { img: "/images/trivias/Beneficios/lineas.png", title: "Líneas de aprendizaje ", description: "donde podrás aprender desde lo básico hasta convertirte en máster.", extra: "" }]
