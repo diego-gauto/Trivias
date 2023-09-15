7b28dfeefac94548d14aa08e7e68bbbb8ef0b6d6
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-15 12:47:39 -0700

    index

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -7,29 +7,31 @@ import { IBlog } from '../../containers/Profile/Blog/IBlog';
 type Repo = {
   data: any,
 }
-export const getServerSideProps: GetServerSideProps<{ blog: Repo }> = async (context: any) => {
-  let tempBlog = [];
-  const id = context.params.slug;
-  const result = await fetch("https://gonvar.inowu.dev/blogs/getBlogs");
-  const allBlogs = await result.json();
-  let leave = "";
-  tempBlog = allBlogs.data.filter((blog: IBlog) => blog.route === id);
-  if (tempBlog.length === 0) {
-    let tempTitle = id.replaceAll("-", " ").replaceAll("%3F?", "?");
-    // tempBlog = allBlogs.data.filter((blog: IBlog) => blog.title === tempTitle);
-    // const sendBlog = await fetch("https://gonvar.inowu.dev/" + "blogs/" + tempBlog[0].id)
-    const blog: any = [];
-    return {
-      props: { blog }
-    }
-  }
-  const sendBlog = await fetch("https://gonvar.inowu.dev/" + "blogs/" + tempBlog[0].id)
-  const blog = await sendBlog.json();
-  return {
-    props: { blog, leave }
-  }
-}
-const BlogScreen = ({ blog }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+// export const getServerSideProps: GetServerSideProps<{ blog: Repo }> = async (context: any) => {
+//   let tempBlog = [];
+//   const id = context.params.slug;
+//   const result = await fetch("https://gonvar.inowu.dev/blogs/getBlogs");
+//   const allBlogs = await result.json();
+//   let leave = "";
+//   tempBlog = allBlogs.data.filter((blog: IBlog) => blog.route === id);
+//   if (tempBlog.length === 0) {
+//     let tempTitle = id.replaceAll("-", " ").replaceAll("%3F?", "?");
+//     // tempBlog = allBlogs.data.filter((blog: IBlog) => blog.title === tempTitle);
+//     // const sendBlog = await fetch("https://gonvar.inowu.dev/" + "blogs/" + tempBlog[0].id)
+//     const blog: any = [];
+//     return {
+//       props: { blog }
+//     }
+//   }
+//   const sendBlog = await fetch("https://gonvar.inowu.dev/" + "blogs/" + tempBlog[0].id)
+//   const blog = await sendBlog.json();
+//   return {
+//     props: { blog, leave }
+//   }
+// }
+
+// { blog }: InferGetServerSidePropsType<typeof getServerSideProps>
+const BlogScreen = () => {
   return (
     <MainContain
       style={{
