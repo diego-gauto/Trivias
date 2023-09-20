85c59333be579ab0ae3e281f0975890ff4a7fb96
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-20 10:54:38 -0600

    INTERface

--- containers/Home/CourseModuleContainer/ICourseModuleContainerProps.ts
+++ containers/Home/CourseModuleContainer/ICourseModuleContainerProps.ts
@@ -1,7 +1,8 @@
+import { ICourse } from "../../../interfaces/ICourse";
 import { IUser } from "../../../interfaces/IUserData";
 
 export interface ICourseModuleContainerProps {
-  courses: any;
+  courses: ICourse;
   num: number;
   loggedIn: boolean;
   user: IUser;
