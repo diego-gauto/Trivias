00707db7268b342c81d3114e6607b15aa1487bc0
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-10-10 13:59:47 -0700

    tags

--- components/HelmetMetaTags/HelmetMetaTags.tsx
+++ components/HelmetMetaTags/HelmetMetaTags.tsx
@@ -14,25 +14,15 @@ export default function HelmetMetaTags(props: DataForHelmet) {
   return (
     <Helmet>
       <title>{title_tag}</title>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="csrf_token" content="" />
-      <meta property="type" content="website" />
       <meta property="url" content={currentUrl} />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="_token" content="" />
-      <meta name="robots" content="noodp" />
       <meta property="title" content={title_tag} />
       <meta property="quote" content={quote_tag} />
       <meta name="description" content={description_tag} />
-      <meta property="image" content={"https://firebasestorage.googleapis.com/v0/b/marketing-gonvar.appspot.com/o/200x200-px.png?alt=media&token=7e24eec7-1117-4ab2-ab0a-7a2acbaf0fa2"} />
-      <meta property="og:locale" content="es_MX" />
-      <meta property="og:type" content="website" />
+      <meta property="image" content={image_tag} />
       <meta property="og:title" content={title_tag} />
       <meta property="og:quote" content={quote_tag} />
       <meta property="og:hashtag" content={hashtag_tag} />
-      <meta property="og:image" content={"https://firebasestorage.googleapis.com/v0/b/marketing-gonvar.appspot.com/o/200x200-px.png?alt=media&token=7e24eec7-1117-4ab2-ab0a-7a2acbaf0fa2"} />
+      <meta property="og:image" content={image_tag} />
       <meta property="og:type" content="article" />
       <meta content="image/*" property="og:image:type" />
       <meta property="og:url" content={currentUrl} />
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,6 @@ import SideBar from "./admin/SideBar";
 import Footer from "./Footer/Footer";
 import HelmetMetaTags from "./HelmetMetaTags/HelmetMetaTags";
 import NavBar from "./NavBar/NavBar";
-
 const Layout = ({ children }: any) => {
   const [isLoading, setIsLoading] = useState(true);
   const responsive1300 = useMediaQuery({ query: "(max-width: 1300px)" });
@@ -17,26 +16,24 @@ const Layout = ({ children }: any) => {
   const { pathname, query, asPath } = router;
   const [path, setPath] = useState(pathname.split('/')[1]);
   const [show, setShow] = useState(false)
-
   useEffect(() => {
     setPath(pathname.split('/')[1]);
     if (pathname === "/_error" && asPath.slice(0, 6) === "/Blogs") {
       router.push("/blogs/" + asPath.slice(6))
     }
   }, [pathname]);
-
   useEffect(() => {
     setTimeout(() => setIsLoading(false), 1000)
   }, []);
   return (
     <Body >
-      <HelmetMetaTags
+      {/* <HelmetMetaTags
         title={"Academia de Belleza Online | Gonvar"}
         image={"https://firebasestorage.googleapis.com/v0/b/marketing-gonvar.appspot.com/o/200x200-px.png?alt=media&token=7e24eec7-1117-4ab2-ab0a-7a2acbaf0fa2"}
         description={"Descubre la academia de belleza para convertirte en un experto. Aprende técnicas y tendencias con los profesionales del sector. ¡Inscríbete ya!"}
         hashtag={"#gonvar"}
         quote={""}
-      />
+      /> */}
       <NavBar />
       <ChildrenContain style={{
         display: router.pathname.slice(1, 6) === "admin" ? "flex" : "initial",
