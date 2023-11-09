368af92d412023168c975fbf9732d771aee380b7
Diego Gauto <diegogauto@yahoo.com.ar>
2023-11-09 12:53:27 -0300

    new forms components

---
+++ components/Forms/inputMail/inputMail.tsx
@@ -0,0 +1,23 @@
+import styles from "./inputMail.module.css";
+
+const InputMail = (props: any) => {
+  const { inputs } = styles;
+  const { label, disabled, placeholder, value, onChange, onBlur } = props;
+
+  return (
+    <div className={inputs}>
+      <label htmlFor="nombre">{label}</label>
+      <input
+        type="text"
+        placeholder={placeholder}
+        name="nombre"
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+
+export default InputMail;
