6301ff4ead5eac9ff953a34e39fa59438f524c5f
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2024-02-01 18:08:06 -0700

    retry payment

--- components/RetryPayment/RetryPayment.styled.ts
+++ components/RetryPayment/RetryPayment.styled.ts
@@ -7,7 +7,7 @@ export const RetryPaymentContainer = styled.div`
   width: 100%;
   padding-block: 30px;
   background: linear-gradient(135deg, #e0d9ea 0%, #f1ebfa 100%);
-  height: 82vh;
+  min-height: 82vh;
   .actives {
     cursor: pointer;
     &:hover {
@@ -53,6 +53,81 @@ export const RetryPaymentContainer = styled.div`
     background-color: #fff;
     border-radius: 40px;
     text-align: center;
+    .payment-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 25px;
+      .payment-method {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        img {
+          width: 50px;
+        }
+        p {
+          color: black;
+        }
+        .dots {
+          font-size: 1.5rem;
+          font-weight: 600;
+          margin-top: 10px;
+        }
+        .text {
+          font-size: 1.3rem;
+          font-weight: 500;
+        }
+      }
+      .edit {
+        display: flex;
+        justify-content: space-between;
+        gap: 15px;
+        width: 400px;
+        align-items: center;
+        p {
+          color: #9a1aff;
+          font-size: 1.2rem;
+          font-weight: 400;
+        }
+        .default {
+          display: flex;
+          gap: 10px;
+          align-items: center;
+          .input-radio {
+            display: flex;
+            width: 30px;
+            height: 20px;
+            border-radius: 100px;
+            background-color: gray;
+            position: relative;
+            transition: 0.4s ease all;
+            .dot {
+              position: absolute;
+              width: 18px;
+              height: 18px;
+              border-radius: 50%;
+              background-color: white;
+              top: 1px;
+              left: 1px;
+            }
+            cursor: pointer;
+          }
+          .selected-radio {
+            background-color: #9a1aff;
+            .dot {
+              left: 11px;
+            }
+          }
+          p {
+            color: #3f1168;
+          }
+        }
+        .right {
+          display: flex;
+          gap: 10px;
+        }
+      }
+    }
     h2 {
       color: #9a1aff;
       margin: 0;
@@ -70,48 +145,6 @@ export const RetryPaymentContainer = styled.div`
     .description-2 {
       font-weight: 600;
       font-size: 1.2rem;
-    }
-    .payment-method {
-      display: flex;
-      gap: 10px;
-      align-items: center;
-      img {
-        width: 50px;
-      }
-      p {
-        color: black;
-      }
-      .dots {
-        font-size: 1.5rem;
-        font-weight: 600;
-        margin-top: 10px;
-      }
-      .text {
-        font-size: 1.3rem;
-        font-weight: 500;
-      }
-    }
-    .edit {
-      display: flex;
-      justify-content: space-between;
-      gap: 15px;
-      width: 400px;
-      p {
-        color: #9a1aff;
-        font-size: 1.2rem;
-        font-weight: 500;
-      }
-      .default {
-        display: flex;
-        gap: 10px;
-        p {
-          color: #3f1168;
-        }
-      }
-      .right {
-        display: flex;
-        gap: 10px;
-      }
     }
   }
 `;
--- components/RetryPayment/RetryPayment.tsx
+++ components/RetryPayment/RetryPayment.tsx
@@ -1,30 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RetryPaymentContainer } from './RetryPayment.styled'
 import { VscChevronDown } from "react-icons/vsc";
 import { RetryPayModal } from '../Modals/RetryPayModal/RetryPayModal';
 const master_card = "/images/RetryPayment/mastercard.png";
-
+let dummyArray: IPm[] = [
+  { last4: '4444', default: true },
+  { last4: '3424', default: false },
+]
+interface IPm {
+  last4: string;
+  default: boolean;
+}
 export const RetryPayment = () => {
+  const [paymentMethods, setPaymentMethods] = useState<IPm[]>(dummyArray)
+  const changePaymentMethod = (index: number) => {
+    let tempPaymentsMethods: IPm[] = [...paymentMethods];
+    // tempPaymentsMethods[index]?.default = false;
+  }
   return (
     <RetryPaymentContainer>
       <div className='main-container'>
         <h2>Métodos de pago</h2>
         <p className='description'>Edita tus métodos de pago y agrega una forma de pago<br /> adicional como respaldo.</p>
-        <div className='payment-method'>
-          <img src={master_card} />
-          <p className='dots'>**** **** ****</p>
-          <p className='text'>{'4811'}</p>
-        </div>
-        <p className='description-2'>Para quitarla, agrega otra forma de pago </p>
-        <div className='edit'>
-          <div className='default'>
-            <p>Predeterminada</p>
+        {
+          paymentMethods.length > 0 &&
+          <div className='payment-container'>
+            {
+              paymentMethods.map((pm: IPm, index: number) => {
+                return (
+                  <div className='payment-container' key={"pm-" + index}>
+                    <div className='payment-method'>
+                      <img src={master_card} />
+                      <p className='dots'>**** **** ****</p>
+                      <p className='text'>{pm.last4}</p>
+                    </div>
+                    <p className='description-2'>Para quitarla, agrega otra forma de pago </p>
+                    <div className='edit'>
+                      <div className='default'>
+                        <div className={'input-radio ' + (pm.default ? "selected-radio" : "")} onClick={() => changePaymentMethod(index)}>
+                          <div className='dot' />
+                        </div>
+                        <p>Predeterminada</p>
+                      </div>
+                      <div className='right'>
+                        <p className='actives'>Editar</p>
+                        <p className='actives'>Eliminar</p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
-          <div className='right'>
-            <p className='actives'>Editar</p>
-            <p className='actives'>Eliminar</p>
-          </div>
-        </div>
+        }
         <button>Reintentar pago</button>
         <button className='type2'>Agregar método de pago <VscChevronDown /> </button>
         <a href='/preview' className='actives'>Ir a mis cursos</a>
