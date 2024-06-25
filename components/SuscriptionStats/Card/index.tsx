fcae00620c88353ebec43b1bf73700be27ed3366
Diego Gauto <diegogauto@yahoo.com.ar>
2024-06-25 11:10:43 -0300

    card component finished

--- components/SuscriptionStats/Card/index.tsx
+++ components/SuscriptionStats/Card/index.tsx
@@ -4,23 +4,40 @@ import React from "react";
 
 import styles from "./cardStats.module.css";
 
+interface Suscriptions {
+  month: number;
+  quarter: number;
+  anual: number
+}
+
 interface CardProps {
   type: string;
   suscriptores: number;
+  suscriptions: Suscriptions;
+  color: string
 }
 
-const CardStats: React.FC<CardProps> = ({ type, suscriptores }) => {
+const CardStats: React.FC<CardProps> = ({ type, suscriptores, suscriptions, color }) => {
+  const { cardContainer, cardBottom, cardTop, title, sub_type, stats, content, plans, subscriber } = styles
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.cardTop}></div>
-      <div className={styles.card}>
-        <h3 className={styles.title}>Suscripciones</h3>
-        <h4>{type}</h4>
-        <div className={styles.content}>
-          <p>{suscriptores}</p>
+    <div className={cardContainer}>
+      <div className={cardBottom} style={{ background: color }}>
+        <div className={cardTop}>
+          <div className={content}>
+            <h3 className={title}>Suscripciones <span className={sub_type}>{type}</span></h3>
+            <div className={stats}>
+              <div className={plans}>
+                <p>Mensuales: {suscriptions.month}</p>
+                <p>Cuatrimestrales: {suscriptions.quarter}</p>
+                <p>Anuales: {suscriptions.anual}</p>
+              </div>
+              <div className={subscriber}>
+                <p>{suscriptores}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.cardBackground}></div>
     </div>
   );
 };
--- containers/SuscriptionStats/index.tsx
+++ containers/SuscriptionStats/index.tsx
@@ -1,11 +1,26 @@
 import CardStats from "../../components/SuscriptionStats/Card";
 import MultiLineChart from "../../components/SuscriptionStats/multiLineChart";
+import styles from "./suscriptionStats.module.css";
 
 const SuscriptionStats = () => {
+  const { container, button_container, button, card_container } = styles
+
   return (
-    <div>
-      <h1>Container Suscription</h1>
-      <CardStats type={"Nuevas"} suscriptores={45} />
+    <div className={container}>
+      <h1>Estadísticas de Suscripciones</h1>
+      <div className={button_container}>
+        <button className={button}>Hoy</button>
+        <button className={button}>Última semana</button>
+        <button className={button}>Último mes</button>
+        <button className={button}>último trimestre</button>
+        <button className={button}>Personalizar fechas</button>
+      </div>
+      <div className={card_container}>
+        <CardStats type={"Nuevas"} suscriptores={45} suscriptions={{ month: 10, quarter: 30, anual: 5 }} color={"#2962FF"} />
+        <CardStats type={"Renovadas"} suscriptores={20} suscriptions={{ month: 5, quarter: 12, anual: 3 }} color={"#E1575A"} />
+        <CardStats type={"Reactivadas"} suscriptores={15} suscriptions={{ month: 3, quarter: 10, anual: 2 }} color={"#F28E2C"} />
+        <CardStats type={"Canceladas"} suscriptores={3} suscriptions={{ month: 1, quarter: 1, anual: 1 }} color={"#40E0D0"} />
+      </div>
       <MultiLineChart />
     </div>
   );
