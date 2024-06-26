4d25aa326a2b83c601032881b8e98b8e660618ac
Diego Gauto <diegogauto@yahoo.com.ar>
2024-06-26 12:20:03 -0300

    create new thankyoupage

---
+++ components/Forms/thankyou/thankYou-dist.tsx
@@ -0,0 +1,59 @@
+// import { useState } from "react";
+
+// import { Background, LoaderContain, LoaderImage } from "../../../screens/Login.styled";
+import styles from "./thankYou-dist.module.css";
+
+const ThankYouFormDist = () => {
+  // const [loading, setLoading] = useState(true);
+
+  const {
+    container,
+    textContainer,
+    title,
+    subtitle,
+    paragraph,
+    imgContainer,
+    contact
+  } = styles;
+
+  // if (loading) {
+  //   return (
+  //     <Background style={{ alignItems: 'center', justifyContent: 'center' }}>
+  //       <LoaderImage>
+  //         <LoaderContain />
+  //       </LoaderImage>
+  //     </Background>
+  //   );
+  // }
+
+  return (
+    <div className={container}>
+      <div className={textContainer}>
+        <p className={title}>¡Felicidades!</p>
+        <p className={subtitle}>Has llenado el formulario con éxito.</p>
+        <p className={paragraph}>
+          ¡Apresúrate! Paga en línea ahora mismo, haciendo click en el botón Comenzar ahora.
+        </p>
+
+        <div className={contact}>
+          <p>Recuerda que nuestros medios de contacto oficiales son:</p>
+          <p>
+            Whatsapp: <b>+52 55 3893 3134</b><br />
+            📞 <b>+1 656-218-5379</b><br />
+            📞 <b>+1 334-560-1678</b><br />
+            📞 <b>55 38 933 134</b><br />
+            Soporte Correo: <b>soporte@gonvar.io</b>
+          </p>
+        </div>
+      </div>
+
+      <div className={imgContainer}>
+        <img
+          alt='Mujer celebrando ser'
+          src='/images/forms/mujerCelebrando.png'
+        />
+      </div>
+    </div>
+  );
+};
+export default ThankYouFormDist;
--- pages/forms/index.tsx
+++ pages/forms/index.tsx
@@ -378,7 +378,7 @@ const Formularios = () => {
         //   form?.redirect?.type === "thankYouPage"
         //     ? "/forms/thankyoupage"
         //     : form?.redirect?.link || "";
-        const link = 'forms/thankyoupage?formId=' + formId;
+        const link = formId === '17' ? 'forms/thankyoupage-dist' : 'forms/thankyoupage?formId=' + formId;
         router.push(link);
 
         // setIsUserCreateModalVisible(true)
