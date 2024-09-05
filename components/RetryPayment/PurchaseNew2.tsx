0eb8f60df59fdc0ab78315f895db0ebfadaafbb5
albertogonvar <alberto@gonvar.io>
2024-09-05 15:41:06 -0600

    Fix context value

--- components/RetryPayment/PurchaseNew2.tsx
+++ components/RetryPayment/PurchaseNew2.tsx
@@ -1040,7 +1040,7 @@ export const PurchaseNew2 = () => {
                                   plan_id: data.subscriptionID,
                                   plan_name: product.title,
                                   start_date: new Date().getTime() / 1000,
-                                  userId: userDataAuth.user.user_id,
+                                  userId: context.user.user_id,
                                   level:
                                     frequency === 'month' || trial === 'true'
                                       ? 1
