dd371b040d659bea3b04c92fe739b7db198df9e0
Diego Gauto <diegogauto@yahoo.com.ar>
2025-10-11 01:46:26 -0300

    envolve useSearchParams completly

--- app/trivias/final/page.tsx
+++ app/trivias/final/page.tsx
@@ -1,43 +1,10 @@
-"use client";
-import React, { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
-import styles from './final.module.css';
-import Failed from '../../../components/Trivias/final/failed';
-import Success from '../../../components/Trivias/final/success';
-import Beneficios from '../../../components/Trivias/final/beneficios';
+import { Suspense } from 'react';
+import FinalContent from './FinalContent';
 
-export default function FinalPage() {
-  const { finalContainer, final, finalTextos, finalVolver, finalImg, link } = styles;
-  const searchParams = useSearchParams();
-  const createUserSuccess = searchParams?.get('createUserSuccess');
-  const success = createUserSuccess === 'true';
-
+export default function Page() {
   return (
     <Suspense fallback={<div>Cargando...</div>}>
-      <>
-        <div className={finalContainer}>
-          <style jsx global>{`
-            body {
-              margin: 0px;
-              padding: 0px;
-            }
-          `}</style>
-          <div className={final}>
-            <Link href={'/trivias'} className={link}>
-              <div className={finalVolver}>
-                <img src='/images/trivias/icono . retroceder.svg' alt='' />
-                <div> Volver</div>
-              </div>
-            </Link>
-            {success ? <Success /> : <Failed />}
-          </div>
-          <div className={finalImg}>
-            <img src='/images/trivias/logo gonvar blanco.svg' alt='' />
-          </div>
-        </div>
-        {success && <Beneficios />}
-      </>
+      <FinalContent />
     </Suspense>
   );
 }
